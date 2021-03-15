38e1d526a22caf6d0233f957ed389411bf91e649
Joran Angevaare <jorana@nikhef.nl>
2021-03-15 12:53:13 +0100

    example of styleguide

--- core_actions/core.py
+++ core_actions/core.py
@@ -1,6 +1,7 @@
 def dummy_list(_max):
     return [i for i in range(_max+1)]
 
+
 def stupid_func():
     """This is a waay too long docstring that just goes on forever, someone needs to come over and pep8 it"""
     pass
