c3cd16fc3918f7542b087f3df2a1c8ad4cd0e571
Joran Angevaare <jorana@nikhef.nl>
2021-03-15 12:55:18 +0100

    add bad style example

--- core_actions/core.py
+++ core_actions/core.py
@@ -5,3 +5,7 @@ def dummy_list(_max):
 def stupid_func():
     """This is a waay too long docstring that just goes on forever, someone needs to come over and pep8 it"""
     pass
+
+def a_new_stupid_func():
+    """A function with bad style"""
+    a = b = 1 ; print(b)
